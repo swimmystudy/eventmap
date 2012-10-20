0d4d6fd722ed03851b45083e6e3c7ff21aba9e1f
milds <tatata2655@gmail.com>
2012-10-20 16:49:41 +0900

    dot cloud started

--- lib/cakephp/app/Config/database.php
+++ lib/cakephp/app/Config/database.php
@@ -10,18 +10,18 @@ class DATABASE_CONFIG {
 		'password' => 'eventmap',
 		'database' => 'eventmap',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
 	public $test = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'eventmap',
+		'password' => 'eventmap',
+		'database' => 'eventmap_test',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 
 	public $twitter = array(
--- lib/cakephp/app/Model/Atnd.php
+++ lib/cakephp/app/Model/Atnd.php
@@ -7,4 +7,48 @@ class Atnd extends Model {
 
 	public $useTable = false;
 
+
+	public function afterFind($results, $primary = false) {
+		if(array_key_exists('events',$results)){
+			return $this->prepareData($results['events']);
+		}
+		return $results;
+	}
+/**
+ * event cache に合わせてデータ返す
+ * 
+ * @param  [type] $results [description]
+ * @return [type]
+ */
+
+	public function prepareData($results){
+		$return = array();
+		foreach ($results as $key => $value) {
+			$tmp = array(
+				'event_id'=>$value['event_id'],
+				'service_provider'=>'Atnd',
+			)
+
+		}
+		var_dump($results);exit;
+	}
+/**
+ * event cash とのマッピング
+ * 
+ * @var array
+ */
+
+    private $schema_map = array(
+        'event_id',
+        'service_provider',
+        'title',
+        'description',
+        'event_url',
+        'started_at',
+        'ended_at',
+        'place',
+        'created',
+        'modified',
+    );
+
 }
